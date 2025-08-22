34e9ac76fd6b354358588ba6c7488ee0fb6c7b9a
Ivan Junior <ivan_junior_we@hotmail.com>
2025-08-22 15:36:26 -0300

    Adicionando o projeto Terraform-AWS-Actions

---
+++ src/main.tf
@@ -0,0 +1,41 @@
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+
+  name = var.aws_vpc_name
+  cidr = var.aws_vpc_cidr
+
+  azs             = var.aws_vpc_azs
+  private_subnets = var.aws_vpc_private_subnets
+  public_subnets  = var.aws_vpc_public_subnets
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
+
+  tags = var.aws_project_tags
+}
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "21.1.0"
+
+#   name               = var.aws_eks_name
+#   kubernetes_version = var.aws_eks_kubernetes_version
+
+#   endpoint_public_access = true
+
+#   enable_cluster_creator_admin_permissions = true
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   eks_managed_node_groups = {
+#     worker_nodes = {
+#       instance_types = var.aws_eks_managed_node_groups_instance_types
+#       min_size       = 2
+#       max_size       = 10
+#       desired_size   = 2
+#     }
+#   }
+# }
---
+++ src/variables.tf
@@ -0,0 +1,59 @@
+variable "aws_region" {
+  description = "Região que os recursos serão criados."
+  type        = string
+  nullable    = false
+}
+
+variable "aws_vpc_name" {
+  description = "Nome da VPC."
+  type        = string
+  nullable    = false
+}
+
+variable "aws_vpc_cidr" {
+  description = "Bloco CIDR para o endereçamento da VPC."
+  type        = string
+  nullable    = false
+}
+
+variable "aws_vpc_azs" {
+  description = "Zonas de disponibilidade."
+  type        = set(string)
+  nullable    = false
+}
+
+variable "aws_vpc_private_subnets" {
+  description = "Endereços das subnets privadas."
+  type        = set(string)
+  nullable    = false
+}
+
+variable "aws_vpc_public_subnets" {
+  description = "Endereços das subnets públicas."
+  type        = set(string)
+  nullable    = false
+}
+
+variable "aws_eks_name" {
+  description = "Nome do cluster EKS."
+  type        = string
+  nullable    = false
+}
+
+variable "aws_eks_kubernetes_version" {
+  description = "Versão do Kubernetes."
+  type        = string
+  nullable    = false
+}
+
+variable "aws_eks_managed_node_groups_instance_types" {
+  description = "Tipos de intâncias dos nodes."
+  type        = set(string)
+  nullable    = false
+}
+
+variable "aws_project_tags" {
+  description = "Tags padrões para o projeto."
+  type        = map(any)
+  nullable    = false
+}
