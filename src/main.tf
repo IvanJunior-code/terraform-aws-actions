5b4834ec8e5465781f4d7c31a0fd1a8a1afc677b
Ivan Junior <33815936+IvanJunior-code@users.noreply.github.com>
2025-08-23 21:40:41 -0300

    Update main.tf

--- src/main.tf
+++ src/main.tf
@@ -44,7 +44,7 @@ module "eks" {
     worker_nodes = {
       instance_types = var.aws_eks_managed_node_groups_instance_types
       ami_type       = "AL2023_x86_64_STANDARD"
-      min_size       = 2
+      min_size       = 3
       max_size       = 10
       desired_size   = 3
 
